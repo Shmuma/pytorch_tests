b8639c19f9cc3185669c8057521a07e1ebc6eb70
Max Lapan <max.lapan@gmail.com>
2017-07-09 13:27:37 +0200

    RNN model

--- char_rnn_classification/char_rnn/model.py
+++ char_rnn_classification/char_rnn/model.py
@@ -1,13 +1,40 @@
 import torch
+import torch.nn as nn
+from torch.autograd import Variable
 
 from . import data
 
+INPUT_SIZE = len(data.ALL_ASCII_LETTERS)
+
 
 def name_to_tensor(name):
-    res = torch.zeros((len(name), 1, len(data.ALL_ASCII_LETTERS)))
+    res = torch.zeros((len(name), 1, INPUT_SIZE))
     for idx, c in enumerate(name):
         pos = data.ALL_ASCII_LETTERS.find(c)
         res[idx][0][pos] = 1
     return res
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax()
+
+    def forward(self, x_input, hidden):
+        combined = torch.cat((x_input, hidden), dim=1)
+        new_hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, new_hidden
+
+    def new_hidden(self):
+        return Variable(torch.zeros(1, self.hidden_size))
 
 pass
--- char_rnn_classification/train_rnn.py
+++ char_rnn_classification/train_rnn.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 # Reimplementation of tutorial from https://github.com/spro/practical-pytorch/tree/master/char-rnn-classification
 
+from torch.autograd import Variable
+
 from char_rnn import data
+from char_rnn import model
+
 
 
 if __name__ == "__main__":
     train_data = data.read_data()
     print("Read %d classes" % len(train_data))
+
+    rnn = model.RNN(input_size=model.INPUT_SIZE, hidden_size=100, output_size=len(train_data))
+    tensor = Variable(model.name_to_tensor("A"))
+    hidden = rnn.new_hidden()
+
+    output, hidden = rnn.forward(tensor[0], hidden)
+    print(output.data.topk(1))
     pass
