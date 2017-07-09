adc426fcc07aaaed00b003d795659eb58b7147de
Max Lapan <max.lapan@gmail.com>
2017-07-09 18:07:30 +0200

    Plots

---
+++ char_rnn_classification/char_rnn/plots.py
@@ -0,0 +1,16 @@
+from bokeh.plotting import figure
+from bokeh.io import output_file, save
+
+
+def plot_data(loss_pairs, file_name):
+    output_file(file_name)
+    f = figure()
+    f.title.text = "Loss"
+    f.plot_width = 1200
+    f.plot_height = 300
+
+    x, y = zip(*loss_pairs)
+    f.line(x, y, color='blue')
+    f.xaxis.axis_label = "Epoch"
+    f.yaxis.axis_label = "LogLoss"
+    save(f)
--- char_rnn_classification/train_rnn.py
+++ char_rnn_classification/train_rnn.py
@@ -8,9 +8,10 @@ from torch import optim
 
 from char_rnn import data
 from char_rnn import model
+from char_rnn import plots
 
-STEPS = 1000000
-PRINT_EVERY_STEP = 10000
+
+EPOCHES = 10
 
 
 if __name__ == "__main__":
@@ -19,7 +20,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     train_data = data.read_data()
-    print("Read %d classes" % len(train_data))
+    epoch_size = sum(map(len, train_data.values()))
+    print("Read %d classes, total samples %d" % (len(train_data), epoch_size))
 
     classes = list(sorted(train_data.keys()))
 
@@ -32,27 +34,27 @@ if __name__ == "__main__":
     loss_sum = 0.0
     losses = []
 
-    for step_idx in tqdm(range(1, STEPS+1)):
-        rnn.zero_grad()
-        v_output = v_hidden = rnn.new_hidden()
+    for epoch in tqdm(range(EPOCHES)):
+        for _ in range(epoch_size):
+            rnn.zero_grad()
+            v_output = v_hidden = rnn.new_hidden()
 
-        sample_class, sample_name, v_class, v_name = model.random_sample(classes, train_data)
-        if args.cuda:
-            v_class = v_class.cuda()
-            v_name = v_name.cuda()
-            v_hidden = v_hidden.cuda()
+            sample_class, sample_name, v_class, v_name = model.random_sample(classes, train_data)
+            if args.cuda:
+                v_class = v_class.cuda()
+                v_name = v_name.cuda()
+                v_hidden = v_hidden.cuda()
 
-        for v_char in v_name:
-            v_output, v_hidden = rnn.forward(v_char, v_hidden)
-        loss = opt_target(v_output, v_class)
-        loss.backward()
-        optimizer.step()
+            for v_char in v_name:
+                v_output, v_hidden = rnn.forward(v_char, v_hidden)
+            loss = opt_target(v_output, v_class)
+            loss.backward()
+            optimizer.step()
 
-        loss_sum += loss.data[0]
-        if step_idx % PRINT_EVERY_STEP == 0:
-            loss_sum /= PRINT_EVERY_STEP
-            print("%d: loss=%.5f" % (step_idx, loss_sum))
-            losses.append(loss_sum)
-            loss_sum = 0.0
-
+            loss_sum += loss.data[0]
+        loss_sum /= epoch_size
+        print("%d: loss=%.5f" % (epoch, loss_sum))
+        losses.append((epoch, loss_sum))
+        loss_sum = 0.0
+        plots.plot_data(losses, "rnn.html")
     pass
