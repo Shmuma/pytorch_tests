8ee482c7924808220c6f3c30ab7a5faaba321b7b
Max Lapan <max.lapan@gmail.com>
2017-07-11 09:50:18 +0200

    Sample works

--- char_rnn_classification/trys/pad_rnn.py
+++ char_rnn_classification/trys/pad_rnn.py
@@ -6,17 +6,18 @@ from torch.autograd import Variable
 
 
 if __name__ == "__main__":
-    t = Variable(torch.FloatTensor([[[1, 2, 3, 4]], [[5, 6, 7, 0]]]))
-    s = t.size()
+    # batch = 2
+    # max_seq_len = 4
+    t = Variable(torch.FloatTensor([[[1], [2], [3], [4]], [[5], [6], [7], [0]]]))
+    print(t.size())
     r = rnn_utils.pack_padded_sequence(t, [4, 3], batch_first=True)
     print(r)
-    rnn = nn.RNN(input_size=4, hidden_size=10, num_layers=1, batch_first=True)
+    rnn = nn.RNN(input_size=1, hidden_size=10, num_layers=1, batch_first=True)
     out, hid = rnn.forward(r, None)
 
 #    print("out", out)
 #    print("hid", hid)
     unpacked, unpacked_len = rnn_utils.pad_packed_sequence(out, batch_first=True)
-    print("Unpacked output")
-    print(unpacked)
-    print("Unpacked output lengths")
-    print(unpacked_len)
+    print("Unpacked output", unpacked.size())
+    print("Unpacked output lengths", unpacked_len)
+    print(hid)
