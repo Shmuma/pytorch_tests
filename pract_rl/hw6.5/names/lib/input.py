c00534683ba14fa2ca4022a5be4b139afaa697ca
Max Lapan <max.lapan@gmail.com>
2017-07-25 06:21:42 +0200

    Start and end tokens

--- pract_rl/hw6.5/names/lib/input.py
+++ pract_rl/hw6.5/names/lib/input.py
@@ -3,17 +3,17 @@ import numpy as np
 TRAIN_FILE = "names"
 
 
-def read_data(file_name=TRAIN_FILE, prepend_space=True):
+def read_data(file_name=TRAIN_FILE):
     with open(file_name, "rt", encoding='utf-8') as fd:
-        res = map(str.strip, fd.readlines())
-        if prepend_space:
-            res = map(lambda s: ' ' + s, res)
-        return list(res)
+        return list(map(str.strip, fd.readlines()))
 
 
 class InputEncoder:
+    START_TOKEN = '\0'
+    END_TOKEN = '\1'
+
     def __init__(self, data):
-        self.alphabet = list(sorted(set("".join(data))))
+        self.alphabet = list(sorted(set("".join(data) + self.END_TOKEN + self.START_TOKEN)))
         self.idx = {c: pos for pos, c in enumerate(self.alphabet)}
 
     def __len__(self):
@@ -24,8 +24,8 @@ class InputEncoder:
 
         if width is None:
             width = len(s)
-        res = np.zeros((width, len(self)), dtype=np.float32)
-        for sample_idx, c in enumerate(s):
+        res = np.zeros((width + 2, len(self)), dtype=np.float32)
+        for sample_idx, c in enumerate(self.START_TOKEN + s + self.END_TOKEN):
             idx = self.idx[c]
             res[sample_idx][idx] = 1.0
 
--- pract_rl/hw6.5/names/tests/test_input.py
+++ pract_rl/hw6.5/names/tests/test_input.py
@@ -6,14 +6,15 @@ from lib import input
 class TestEncoder(unittest.TestCase):
     def test_simple(self):
         enc = input.InputEncoder(['abc'])
-        self.assertEqual(3, len(enc))
+        self.assertEqual(5, len(enc))       # 3 + start + end
 
         d = enc.encode("aaa")
-        self.assertEqual(d.shape, (3, len(enc)))
+        self.assertEqual(d.shape, (5, len(enc)))
         self.assertEqual(d[0][0], 1.0)
+        self.assertEqual(d[1][2], 1.0)
 
         d = enc.encode("a", width=4)
-        self.assertEqual(d.shape, (4, len(enc)))
+        self.assertEqual(d.shape, (6, len(enc)))
 
 
 if __name__ == '__main__':
