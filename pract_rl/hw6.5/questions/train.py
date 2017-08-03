5783becf09ac3e85fcb08671da5bd1a6619f4c82
Max Lapan <maxl@fornova.net>
2017-08-03 16:47:18 +0200

    Skip empty questions

--- pract_rl/hw6.5/questions/train.py
+++ pract_rl/hw6.5/questions/train.py
@@ -111,7 +111,10 @@ if __name__ == "__main__":
         epoch_losses.append(loss)
 
         log.info("Epoch %d: mean_loss=%.4f, speed=%.3f item/s", epoch, loss, speed)
-        question = model.generate_question(net, net_map, words, rev_words, args.cuda)
+        while True:
+            question = model.generate_question(net, net_map, words, rev_words, args.cuda)
+            if question:
+                break
         print("Question on epoch %d: %s" % (epoch, " ".join(question)))
         sys.stdout.flush()
         plots.plot_progress(epoch_losses, os.path.join(save_path, "status.html"))
