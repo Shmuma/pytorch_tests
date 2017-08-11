aff3b3230f74657210fb427c0cf6bd954049b65c
Max Lapan <maxl@fornova.net>
2017-08-11 11:53:57 +0200

    Add timings

--- pract_rl/hw6.5/chemistry/train.py
+++ pract_rl/hw6.5/chemistry/train.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import os
+import time
 import argparse
+import collections
 import itertools
 import logging
 import random
@@ -72,21 +74,31 @@ if __name__ == "__main__":
 
     for epoch in range(EPOCHES):
         losses = []
+        time_counters = collections.Counter()
+        ts_epoch = time.time()
+
         for batch in tqdm(input.iterate_batches(train_data, BATCH_SIZE), total=len(train_data) / BATCH_SIZE):
+            ts = time.time()
             optimizer.zero_grad()
             input_packed, output_sequences = input.encode_batch(batch, input_vocab, cuda=args.cuda)
+            time_counters['prepare_batch'] += time.time() - ts
 
+            ts = time.time()
             hid = encoder(input_packed)
+            time_counters['encoder'] += time.time() - ts
 
             # input for decoder
+            ts = time.time()
             input_token_indices = np.array([end_token_idx]*len(batch))
             max_out_len = max(map(len, output_sequences))
             # expected tokens
             valid_token_indices = np.full(shape=(len(batch), max_out_len), fill_value=end_token_idx, dtype=np.int64)
             for idx, out_seq in enumerate(output_sequences):
                 valid_token_indices[idx][:len(out_seq)] = [output_vocab.token_index[c] for c in out_seq]
+            time_counters['expected_tokens'] += time.time() - ts
 
             # iterate decoder over largest sequence
+            ts = time.time()
             for ofs in range(max_out_len):
                 input_emb = np.zeros(shape=(len(batch), len(output_vocab)), dtype=np.float32)
                 input_emb[:, input_token_indices] = 1.0
@@ -106,8 +118,13 @@ if __name__ == "__main__":
                 loss = nn_func.cross_entropy(dec_out, valid_idx_v)
                 loss.backward(retain_variables=True)
                 losses.append(loss.cpu().data.numpy())
+            time_counters['decoder'] += time.time() - ts
             optimizer.step()
+        ts_epoch = time.time() - ts_epoch
         log.info("Epoch %d: mean_loss=%.4f", epoch, np.mean(losses))
         epoch_losses.append(np.mean(losses))
         plots.plot_progress(epoch_losses, os.path.join(save_path, "status.html"))
+        log.info("Timings:")
+        for name, ts in time_counters.most_common():
+            log.info("  %s: %.3f seconds, %.2f%%", name, ts, 100.0 * ts / ts_epoch)
     pass
