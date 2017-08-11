79fa17d9480ea2417d5bd7bda2da3724d79abf08
Max Lapan <maxl@fornova.net>
2017-08-11 16:35:13 +0200

    Reorg

--- pract_rl/hw6.5/chemistry/train.py
+++ pract_rl/hw6.5/chemistry/train.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     train_data, test_data = input.split_train_test(data, ratio=0.9)
     if args.tiny:
         train_data = train_data[:5000]
-    random.shuffle(train_data)
+    train_data.sort(key=len)
     log.info("Train has %d items, test %d", len(train_data), len(test_data))
 
     # train
@@ -74,47 +74,39 @@ if __name__ == "__main__":
 
     for epoch in range(EPOCHES):
         losses = []
-        time_counters = collections.Counter()
-        ts_epoch = time.time()
 
         for batch in tqdm(input.iterate_batches(train_data, BATCH_SIZE), total=len(train_data) / BATCH_SIZE):
-            ts = time.time()
             optimizer.zero_grad()
             input_packed, output_sequences = input.encode_batch(batch, input_vocab, cuda=args.cuda)
-            time_counters['prepare_batch'] += time.time() - ts
 
-            ts = time.time()
             hid = encoder(input_packed)
-            time_counters['encoder'] += time.time() - ts
 
             # input for decoder
-            ts = time.time()
-            input_token_indices = torch.LongTensor([[end_token_idx]] * len(batch))
+            input_token_indices = torch.LongTensor([end_token_idx]).repeat(len(batch), 1)
             max_out_len = max(map(len, output_sequences)) + 1 # extra item for final END_TOKEN
-            # expected tokens
-            valid_token_indices = np.full(shape=(len(batch), max_out_len), fill_value=end_token_idx, dtype=np.int64)
+            # expected tokens. We encode it at once and transposed, so our valid indices will be at rows
+            # TODO: masking https://discuss.pytorch.org/t/how-can-i-compute-seq2seq-loss-using-mask/861/16
+            valid_token_indices = np.full(shape=(max_out_len, len(batch)), fill_value=end_token_idx, dtype=np.int64)
             for idx, out_seq in enumerate(output_sequences):
-                valid_token_indices[idx][:len(out_seq)] = [output_vocab.token_index[c] for c in out_seq]
-            valid_token_indices_v = Variable(torch.from_numpy(valid_token_indices.transpose()))
-            time_counters['expected_tokens'] += time.time() - ts
+                valid_token_indices[:len(out_seq), idx] = [output_vocab.token_index[c] for c in out_seq]
+            valid_token_indices_v = Variable(torch.from_numpy(valid_token_indices))
 
             input_emb = torch.FloatTensor(len(batch), len(output_vocab))
-            input_emb_v = Variable(input_emb)
             if args.cuda:
-                input_emb_v = input_emb_v.cuda()
+                input_emb = input_emb.cuda()
                 input_token_indices = input_token_indices.cuda()
                 valid_token_indices_v = valid_token_indices_v.cuda()
 
             # iterate decoder over largest sequence
             batch_loss = None
 
-            ts = time.time()
             for ofs in range(max_out_len):
-                input_emb_v.data.zero_()
-                input_emb_v.data.scatter_(1, input_token_indices, 1.0)
+                # fill input embeddings with one-hot
+                input_emb.zero_()
+                input_emb.scatter_(1, input_token_indices, 1.0)
 
                 # on first iteration pass hidden from encoder
-                dec_out, hid = decoder(input_emb_v, hid)
+                dec_out, hid = decoder(Variable(input_emb), hid)
                 # sample next tokens for decoder's input
                 input_token_indices = torch.multinomial(nn_func.softmax(dec_out), num_samples=1).data
                 loss = nn_func.cross_entropy(dec_out, valid_token_indices_v[ofs])
@@ -122,15 +114,10 @@ if __name__ == "__main__":
                     batch_loss = loss
                 else:
                     batch_loss += loss
-            time_counters['decoder'] += time.time() - ts
             batch_loss.backward()
             optimizer.step()
             losses.append(batch_loss.cpu().data.numpy() / max_out_len)
-        ts_epoch = time.time() - ts_epoch
         log.info("Epoch %d: mean_loss=%.4f", epoch, np.mean(losses))
         epoch_losses.append(np.mean(losses))
         plots.plot_progress(epoch_losses, os.path.join(save_path, "status.html"))
-        log.info("Timings:")
-        for name, ts in time_counters.most_common():
-            log.info("  %s: %.3f seconds, %.2f%%", name, ts, 100.0 * ts / ts_epoch)
     pass
