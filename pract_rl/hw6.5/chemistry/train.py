f480d3a0f95034f7a840191a6a07ac0facc95712
Max Lapan <maxl@fornova.net>
2017-08-26 09:48:36 +0200

    Fix test problems

--- pract_rl/hw6.5/chemistry/train.py
+++ pract_rl/hw6.5/chemistry/train.py
@@ -36,38 +36,43 @@ log = logging.getLogger("train")
 
 
 def test_model(net_encoder, net_decoder, input_vocab, output_vocab, test_data, cuda=False):
-    input_packed, output_sequences = input.encode_batch(test_data, input_vocab, cuda=cuda, volatile=True)
-    hid = net_encoder(input_packed)
-    end_token_idx = output_vocab.token_index[input.END_TOKEN]
-    input_token_indices = torch.LongTensor([end_token_idx]).repeat(len(test_data), 1)
-    max_out_len = max(map(len, output_sequences)) + 1  # extra item for final END_TOKEN
-    input_emb = torch.FloatTensor(len(test_data), len(output_vocab))
-
-    if cuda:
-        input_emb = input_emb.cuda()
-        input_token_indices = input_token_indices.cuda()
-
     total_tokens = 0
     valid_tokens = 0
 
-    for ofs in range(max_out_len):
-        input_emb.zero_()
-        input_emb.scatter_(1, input_token_indices, 1.0)
-        input_emb_v = Variable(input_emb, volatile=True)
-        if cuda:
-            input_emb_v = input_emb_v.cuda()
+    batch_bounds = input.split_batches(test_data, BATCH_SIZE)
+    for batch_start, batch_end in tqdm(batch_bounds):
+        batch = data[batch_start:batch_end]
 
-        dec_out, hid = net_decoder(input_emb_v, hid)
-        input_token_indices = torch.multinomial(nn_func.softmax(dec_out), num_samples=1).data
-        for seq_idx, token in enumerate(input_token_indices):
-            cur_seq = output_sequences[seq_idx]
-            if ofs > len(cur_seq):
-                continue
-            total_tokens += 1
-            if ofs == len(cur_seq):
-                valid_tokens += int(token[0] == end_token_idx)
-            else:
-                valid_tokens += int(token[0] == output_vocab.token_index[cur_seq[ofs]])
+        input_packed, output_sequences = input.encode_batch(batch, input_vocab, cuda=cuda, volatile=True)
+        hid = net_encoder(input_packed)
+        end_token_idx = output_vocab.token_index[input.END_TOKEN]
+        input_token_indices = torch.LongTensor([end_token_idx]).repeat(len(batch), 1)
+        max_out_len = max(map(len, output_sequences)) + 1  # extra item for final END_TOKEN
+        input_emb = torch.FloatTensor(len(batch), len(output_vocab))
+
+        if cuda:
+            input_emb = input_emb.cuda()
+            input_token_indices = input_token_indices.cuda()
+
+
+        for ofs in range(max_out_len):
+            input_emb.zero_()
+            input_emb.scatter_(1, input_token_indices, 1.0)
+            input_emb_v = Variable(input_emb, volatile=True)
+            if cuda:
+                input_emb_v = input_emb_v.cuda()
+
+            dec_out, hid = net_decoder(input_emb_v, hid)
+            input_token_indices = torch.multinomial(nn_func.softmax(dec_out), num_samples=1).data
+            for seq_idx, token in enumerate(input_token_indices):
+                cur_seq = output_sequences[seq_idx]
+                if ofs > len(cur_seq):
+                    continue
+                total_tokens += 1
+                if ofs == len(cur_seq):
+                    valid_tokens += int(token[0] == end_token_idx)
+                else:
+                    valid_tokens += int(token[0] == output_vocab.token_index[cur_seq[ofs]])
     return valid_tokens / total_tokens
 
 
@@ -197,7 +202,7 @@ if __name__ == "__main__":
                 optimizer.step()
                 losses.append(batch_loss.cpu().data.numpy())
         test_ratio = test_model(encoder, decoder, input_vocab, output_vocab, test_data, cuda=args.cuda)
-        train_ratio = test_model(encoder, decoder, input_vocab, output_vocab, train_data, cuda=args.cuda)
+        train_ratio = test_model(encoder, decoder, input_vocab, output_vocab, train_data[:len(test_data)], cuda=args.cuda)
         speed = total_tokens / (time.time() - ts)
         log.info("Epoch %d: mean_loss=%.4f, test_ratio=%.3f%%, train_ratio=%.3f%%, speed=%.3f tokens/s",
                  epoch, np.mean(losses), test_ratio * 100.0, train_ratio * 100.0, speed)
