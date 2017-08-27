dcd04c818a019c563f1fc091e4aeeb8adcfd7d2d
Max Lapan <maxl@fornova.net>
2017-08-27 21:09:53 +0200

    Try rmsprom

--- pract_rl/hw6.5/chemistry/train.py
+++ pract_rl/hw6.5/chemistry/train.py
@@ -132,7 +132,7 @@ if __name__ == "__main__":
     if args.cuda:
         encoder.cuda()
         decoder.cuda()
-    optimizer = optim.Adam(itertools.chain(encoder.parameters(), decoder.parameters()), lr=0.001)
+    optimizer = optim.RMSprop(itertools.chain(encoder.parameters(), decoder.parameters()), lr=0.001)
 
     end_token_idx = output_vocab.token_index[input.END_TOKEN]
     epoch_losses = []
